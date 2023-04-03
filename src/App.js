c5877f65b0f91adf902a548e2d7318acc75332f5
NikeshPakuwal <pakuwalnikesh@gmail.com>
2023-04-03 22:03:18 +0545

    props system image handeling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
+
 
 function App(){
     return(
         <div>
-            <div>Personal Digital Assistants</div>
+            <div><b>Personal Digital Assistants</b></div>
 
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <ProfileCard title="Alexa" handel="@alexa99" image={AlexaImage}/>
+            <ProfileCard title="Cortana" handel="@cortana32" image={CortanaImage}/>
+            <ProfileCard title="Siri" handel="@siri01" image={SiriImage}/>
         </div>
     )
 }
