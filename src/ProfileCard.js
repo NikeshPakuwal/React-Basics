53d8dd7091cb403a89233f2905db7fad9ed5ce6d
NikeshPakuwal <pakuwalnikesh@gmail.com>
2023-04-04 15:09:47 +0545

    Bundle instialization and styling with bundle html

--- src/ProfileCard.js
+++ src/ProfileCard.js
@@ -1,10 +1,20 @@
-function ProfileCard({title, handel, image}){
+function ProfileCard({title, handel, image, description}){
 
     return (
-        <div>
-            <img src={image} />
-            <div>Title is {title}</div>
-            <div>Handle is {handel}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure>
+                    <img src={image} alt="pda logo" />
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="title is-6">{handel}</p>
+                </div>
+                <div className="content">{description}</div>
+            </div>
         </div>
     );
 }
